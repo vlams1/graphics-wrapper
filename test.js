2fed2bc81a36667529a03cfb2bef094d0bbd5788
Vlams <flvlams@gmail.com>
2020-03-05 15:33:13 +0100

    Added test script

---
+++ test.js
@@ -0,0 +1,13 @@
+﻿﻿﻿const fs = require('fs');
+const Canvas = require('canvas');
+const Draw = require('../graphics-wrapper');
+
+const canvas = Canvas.createCanvas(500, 500);
+const ctx = canvas.getContext('2d');
+
+Draw.setContext(ctx);
+Draw.drawRectFill('black', 0, 0, canvas.width, canvas.height);
+Draw.drawRoundedRectFill('white', 50, 50, canvas.width - 100, canvas.height - 100, 50);
+
+let buffer = canvas.toBuffer();
+fs.writeFileSync("test.png", buffer);
