752cbbf611d25114425da7e9c46c678a8d502f01
Vlams <flvlams@gmail.com>
2020-03-05 15:36:41 +0100

    Changed test require

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 ﻿﻿﻿const fs = require('fs');
 const Canvas = require('canvas');
-const Draw = require('../graphics-wrapper');
+const Draw = require('./');
 
 const canvas = Canvas.createCanvas(500, 500);
 const ctx = canvas.getContext('2d');
